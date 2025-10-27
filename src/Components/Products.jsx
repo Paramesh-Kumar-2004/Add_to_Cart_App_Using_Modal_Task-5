e17e0e6b053263f30a3e5f04883c6c3d30f0f64c
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-27 23:36:02 +0530

    Working On It

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -12,19 +12,23 @@ export const Products = () => {
     const fetchData = async () => {
         try {
             const response = await axios.get("https://fakestoreapi.com/products")
+            console.log(response)
             setItems(response.data)
+            console.log("Response :", response.data)
         } catch (error) {
             console.log(error)
         }
     }
 
     return (
-        <div>
+        <div className='flex gap-5 flex-wrap justify-around'>
 
             {items.map((item) => {
                 return (
-                    <div key={item.id} >
-                        <img src={item.image} alt="image" />
+                    <div key={item.id} className='h-auto w-96 bg-amber-400'>
+                        <img src={item.image} alt="img" className=''/>
+                        <h1 className='p-3 text-center'>{item.title}</h1>
+                        <h1 className='p-3 text-center'>{item.price}</h1>
                     </div>
                 )
             })}
