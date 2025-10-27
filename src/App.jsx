0e3b0c53066675acb139ea18c01f9f1afa3c2c2a
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-27 22:23:28 +0530

    Product List Start

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Products } from './Components/Products'
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <h1>VP</h1>
+      <Products />
     </>
   )
 }
---
+++ src/Components/Products.jsx
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+export const Products = () => {
+
+    const [items, setItems] = useState([])
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get("https://fakestoreapi.com/products")
+            setItems(response.data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <div>
+
+            {items.map((item) => {
+                return (
+                    <div key={item.id}>
+                        <img src={item.image} alt="image" />
+                    </div>
+                )
+            })}
+
+        </div>
+    )
+
+}
