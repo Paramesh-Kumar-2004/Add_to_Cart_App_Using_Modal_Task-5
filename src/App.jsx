3550459e27b3abc35891fd16e9e73b28d1fb56e8
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-28 22:17:57 +0530

    Cart Image Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Products } from './Components/Products'
-
+import CartPng from "./assets/Cart.png"
 
 function App() {
 
@@ -10,7 +10,10 @@ function App() {
     <div className='bg-sky-100'>
       <div className='flex p-3 gap-3 justify-around'>
         <h1 className='font-bold'>VP</h1>
-        <h1 className='font-bold'>Cart : {count}</h1>
+        <div>
+          <img src={CartPng} alt="cart" width={52} />
+          <p>{count}</p>
+        </div>
       </div>
       <Products count={count} setCount={setCount} />
     </div>
