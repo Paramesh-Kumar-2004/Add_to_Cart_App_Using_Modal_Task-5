88b67e313709e46e63e62837183ae5f03ea884e2
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-29 12:47:30 +0530

    Working Cart Modal Cards Styling

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 
-const ProductCard = ({ items }) => {
+
+const ProductCard = ({ items, HandleCart }) => {
+
     return (
         <div>
-            {items.map((item) => {
-                return (
-                    <div key={item.id} className='flex flex-col justify-around rounded-md items-center h-auto w-96 bg-sky-300 border-2 border-sky-800 p-3'>
-                        <h1 className='p-3 text-center'>{item.title}</h1>
-                        <img src={item.image} alt="img" className='w-32' />
-                        {/* <p className='break-all p-3'>{item.description}</p> */}
-                        <h1 className='p-3 text-center'>Price : ${item.price}</h1>
-                        <button className='bg-green-500 p-3 text-black hover:cursor-pointer font-extrabold rounded-xl' onClick={() => HandleCart(item.id)}>Add To Card</button>
-                    </div>
-                )
-            })}
+            <div className='flex gap-5 flex-wrap justify-around overflow-scroll'>
+                {items.map((item) => {
+                    return (
+                        <div key={item.id} className='flex flex-col justify-around rounded-md items-center h-auto w-96 bg-sky-300 border-2 border-sky-800 p-3'>
+                            <h1 className='p-3 text-center'>{item.title}</h1>
+                            <img src={item.image} alt="img" className='w-32' />
+                            {/* <p className='break-all p-3'>{item.description}</p> */}
+                            <h1 className='p-3 text-center'>Price : ${item.price}</h1>
+                            <button className='bg-green-500 p-3 text-black hover:cursor-pointer font-extrabold rounded-xl' onClick={() => HandleCart(item.id)}>Add To Card</button>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
+
 }
 
 export default ProductCard
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,6 +2,9 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 
+import ProductCard from '../Components/ProductCard'
+
+
 
 export const Products = () => {
 
@@ -46,22 +49,21 @@ export const Products = () => {
 
     return (
         <div>
-            <div className='flex gap-5 flex-wrap justify-around'>
 
-                {/* {items.map((item) => {
+            <div className='flex gap-5 flex-wrap justify-around overflow-scroll'>
+                {items.map((item) => {
                     return (
                         <div key={item.id} className='flex flex-col justify-around rounded-md items-center h-auto w-96 bg-sky-300 border-2 border-sky-800 p-3'>
                             <h1 className='p-3 text-center'>{item.title}</h1>
                             <img src={item.image} alt="img" className='w-32' />
-                            <p className='break-all p-3'>{item.description}</p>
+                            {/* <p className='break-all p-3'>{item.description}</p> */}
                             <h1 className='p-3 text-center'>Price : ${item.price}</h1>
                             <button className='bg-green-500 p-3 text-black hover:cursor-pointer font-extrabold rounded-xl' onClick={() => HandleCart(item.id)}>Add To Card</button>
                         </div>
                     )
-                })} */}
-
-
+                })}
             </div>
+
         </div>
     )
 
