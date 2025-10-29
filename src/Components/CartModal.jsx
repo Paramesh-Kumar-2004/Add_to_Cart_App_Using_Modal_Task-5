1bb4f75ffc25d2b701fade8e7cdea85ce82481e4
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-29 13:37:59 +0530

    Cart Items (Modal) Shows Correctly

--- src/Components/CartModal.jsx
+++ src/Components/CartModal.jsx
@@ -1,27 +1,48 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 
 
 const CartModal = () => {
 
-    const cartItems = JSON.parse(localStorage.getItem("cartItems")) || []
-    console.log(cartItems)
+    const [totalAmount, setTotalAmount] = useState(0)
+    const [products, setProducts] = useState([])
+
+    const productId = JSON.parse(localStorage.getItem("cartItems")) || []
+    console.log(productId)
+
+    useEffect(() => {
+        fetchProducts()
+    }, [totalAmount])
+
+    const fetchProducts = async () => {
+        try {
+            console.log("Log");
+            const response = await axios.get("https://fakestoreapi.com/products")
+            setProducts(response.data)
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
     const HandleRemoveCart = async (id) => {
+        setTotalAmount(totalAmount => totalAmount += 1)
         console.log(id)
         toast.info(id, {
             position: "top-center"
         })
     }
 
+    const filteredProducts = products.filter((item) => productId.includes(item.id))
+    console.log("Filtered Items :", filteredProducts)
+
     return (
         <div>
             <div className="min-h-96 max-h-[80vh] mt-6 lg:w-[1000px] w-full min-w-2xs flex-1 bg-gray-300 rounded-md shadow-lg flex flex-wrap gap-3 overflow-y-scroll [scrollbar-width:none] justify-around items-center">
 
-                {cartItems.map((item) => {
+                {filteredProducts.map((item) => {
                     return (
-                        <div key={item.id} className='flex flex-col flex-1 justify-around rounded-md items-center min-h-64 min-w-3xs w-72 bg-sky-300 border-2 border-sky-800 p-3 m-3'>
-                            <h1 className='p-3 text-center'>{item}</h1>
+                        <div key={item.id} className='flex flex-col flex-1 justify-around rounded-md items-center h-96 min-w-3xs w-72 bg-sky-300 border-2 border-sky-800 p-3 m-3'>
                             <h1 className='p-3 text-center'>{item.title}</h1>
                             <img src={item.image} alt="img" className='w-32' />
                             {/* <p className='break-all p-3'>{item.description}</p> */}
