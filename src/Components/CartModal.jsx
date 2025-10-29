5db8cd9e3e81dcf98c508cf31953986c158a3300
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-10-29 23:06:48 +0530

    Cart Is Completed

--- src/Components/CartModal.jsx
+++ src/Components/CartModal.jsx
@@ -33,11 +33,11 @@ const CartModal = ({ HandleCartModal }) => {
         const updatedCart = productId.filter((itemId) => itemId !== id);
         localStorage.setItem("cartItems", JSON.stringify(updatedCart));
         window.dispatchEvent(new Event("cartUpdated"));
-        toast.info(`Removed item with id: ${id}`, {
+
+        toast.warn(`Removed item with id: ${id}`, {
             position: "top-center",
         });
     };
-
 
     const filteredProducts = products.filter((item) => productId.includes(item.id))
     const Amount = filteredProducts.reduce((acc, item) => acc + item.price, 0)
@@ -53,28 +53,33 @@ const CartModal = ({ HandleCartModal }) => {
     return (
         <div className='h-screen w-full mt-6 p-4 flex-1 bg-gray-300 rounded-md shadow-lg flex flex-col flex-wrap gap-3 overflow-y-scroll [scrollbar-width:none] justify-around items-center'>
 
-            <div className='flex flex-col justify-around items-center'>
-                <div className='w-full flex justify-between items-center border-b-2 border-amber-400 mb-2'>
+            <div className='w-full flex flex-col justify-around items-center'>
+                <div className='fixed top-5 px-3 py-2 w-full flex justify-between items-center border-b-2 border-red-800 mb-2'>
                     <h1>Total Amount : <b>{Amount}</b></h1>
                     <button onClick={HandleCartModal} className='p-2 m-2 text-white bg-red-600 rounded-md hover:cursor-pointer'>X</button>
                 </div>
                 <div className='flex gap-5 flex-wrap justify-around'>
-                    {filteredProducts.map((item) => {
-                        return (
-                            <div key={item.id} className='flex flex-col justify-around rounded-md items-center h-auto w-96 bg-sky-300 border-2 border-sky-800 p-3'>
-                                <h1 className='p-3 text-center'>{item.title}</h1>
-                                <img src={item.image} alt="img" className='w-32' />
-                                {/* <p className='break-all p-3'>{item.description}</p> */}
-                                <h1 className='p-3 text-center'>Price : ${item.price}</h1>
-                                <button className='bg-green-500 p-3 text-black hover:cursor-pointer font-extrabold rounded-xl' onClick={() => HandleRemoveCart(item.id)}>Remove From Cart</button>
+                    {filteredProducts.length > 0 ? (
+                        filteredProducts.map((item) => (
+                            <div key={item.id} className="flex flex-col justify-around rounded-md items-center h-auto w-96 bg-sky-300 border-2 border-sky-800 p-3" >
+                                <h1 className="p-3 text-center">{item.title}</h1>
+                                <img src={item.image} alt="img" className="w-32" />
+                                <h1 className="p-3 text-center">Price : ${item.price}</h1>
+                                <button className="bg-green-500 p-3 text-black hover:cursor-pointer font-extrabold rounded-xl" onClick={() => HandleRemoveCart(item.id)} >
+                                    Remove From Cart
+                                </button>
                             </div>
-                        )
-                    })}
+                        ))
+                    ) : (
+                        <h2 className="text-xl font-semibold text-gray-700 mt-10">
+                            No Items In Your Cart.
+                        </h2>
+                    )}
                 </div>
             </div>
 
 
-        </div>
+        </div >
     )
 }
 
